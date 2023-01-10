91b19d873efa43195e7710a41018bddeff4cc32b
kremniland <kremnilandk@gmail.com>
2023-01-10 15:34:12 +0300

    Отпадал бутстрап исправил

--- config/urls.py
+++ config/urls.py
@@ -4,15 +4,20 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings # Правильный импорт settings подтянет все настройки
 
+from prostoapp.views import IndexView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('prostoapp.urls', namespace='prostoapp'), name='prostoapp'),
+    path('', IndexView.as_view(), name='index'),
+
+    path('products/', include('prostoapp.urls', namespace='prostoapp')),
     path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG == True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
--- prostoapp/views.py
+++ prostoapp/views.py
@@ -1,27 +1,54 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.views.generic import TemplateView, ListView
 
 from .models import Product, ProductCategory
 
 
-def index(request):
-    context = {}
-    return render(request, 'prostoapp/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'prostoapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
 
 
-def products(request, category_id=None, page_number=1):
-    # if category_id:
-    #     products = Product.objects.filter(category_id=category_id)
-    # else:
-    #     products = Product.objects.all()
-    # ==
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-    context = {
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'prostoapp/products.html', context)
+# def index(request):
+#     context = {'title': 'Главная страница'}
+#     return render(request, 'prostoapp/index.html', context)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'prostoapp/products.html'
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        # Если category_id есть в запросе выведет по категории иначе весь список
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+# def products(request, category_id=None, page_number=1):
+#     # if category_id:
+#     #     products = Product.objects.filter(category_id=category_id)
+#     # else:
+#     #     products = Product.objects.all()
+#     # ==
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     per_page = 3
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_number)
+#     context = {
+#         'products': products_paginator,
+#         'categories': ProductCategory.objects.all(),
+#     }
+#     return render(request, 'prostoapp/products.html', context)
 
